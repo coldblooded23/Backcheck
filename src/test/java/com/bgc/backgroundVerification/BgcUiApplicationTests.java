ca05d06749a7bcd76ba986d09b96252c6d609f68
Cstech-Java <75626485+Cstech-Java@users.noreply.github.com>
2021-12-20 16:05:59 +0530

    By Koti

---
+++ src/test/java/com/bgc/backgroundVerification/BgcUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgc.backgroundVerification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgcUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
